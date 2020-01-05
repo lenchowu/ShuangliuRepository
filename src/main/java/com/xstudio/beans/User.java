7acd8fd0640041fa9facefd3c0f90c6df2f73d7e
lencho wu <lencho.wu@foxmail.com>
2020-01-05 21:48:59 +0800

    create user bean

---
+++ src/main/java/com/xstudio/beans/User.java
@@ -0,0 +1,40 @@
+package com.xstudio.beans;
+
+public class User {
+    private String UserID;
+    private String userName;
+    private String loginName;
+    private String loginPassword;
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getLoginPassword() {
+        return loginPassword;
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+}
